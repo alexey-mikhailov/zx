48b54e9406c16caa711411a0797224bf570f89cd
Alexey <lex_87@mail.ru>
2021-07-06 16:39:46 +0300

    zx::type: keeping several factory methods. Fixed zx::type::i method.

--- core/zx_type.h
+++ core/zx_type.h
@@ -29,10 +29,10 @@ namespace zx
 	class type
 	{
 		type_data *_data;
-		ZX_API static std::map<std::type_index, type *> __instances;
+		ZX_API static std::map<std::type_index, zx::type *> __instances;
 
 	public:
-		ZX_API static type null;
+		ZX_API static zx::type null;
 
 		// Properties
 		const std::type_index& index = _data->index;
@@ -40,52 +40,71 @@ namespace zx
 		const bool& is_pointer = _data->is_pointer;
 		const bool& is_abstract = _data->is_abstract;
 		
-		ZX_API type(const type &other);
+		ZX_API type(const zx::type &other);
 
 		/// Gets instance of abstract type. 
-		template <class T, class... Args>
-		static typename std::enable_if<
-			std::is_abstract<T>::value, const type &>::type
-		i()
+		template <class T>
+		static const zx::type& i()
 		{
 			const auto it = __instances.find(typeid(T));
 			if (it == __instances.end())
 			{
 				auto value = new type(type_data::create<T>());
 				__instances[typeid(T)] = value;
+
+				if constexpr (std::is_default_constructible_v<T>)
+				{
+					const rtti_args arg_types = rtti_pack<>::get();
+
+					auto& fms = value->_data->factory_methods;
+					const auto fit = fms.find(arg_types);
+
+					if (fit == fms.end())
+					{
+						void* (*temp)() =
+						[]() -> void*
+						{
+							return new T();
+						};
+
+						fms[arg_types] = reinterpret_cast<fnptr>(temp);
+					}
+				}
+
 				return *value;
 			}
 
 			return *it->second;
 		}
 
-		/// Gets instance of concrete type. 
-		template <class T, class... Args>
-		static typename	std::enable_if<
-			!std::is_abstract<T>::value, const type &>::type
-		i()
+		template <typename T, typename ReflCtor>
+		static const type& ensure()
 		{
 			const auto it = __instances.find(typeid(T));
 			if (it == __instances.end())
 			{
-				auto data = type_data::create<T>();
-				auto& fms = data->factory_methods;
-				const rtti_args arg_types = rtti_pack<Args...>::get();
-				const auto fit = fms.find(arg_types);
+				auto value = new type(type_data::create<T>());
+				__instances[typeid(T)] = value;
 
-				if (fit == fms.end())
+				if constexpr (std::is_default_constructible_v<T>)
 				{
-					void* (*temp)(Args...) = 
-						[](Args... args) -> void*
+					const rtti_args arg_types = rtti_pack<>::get();
+
+					auto& fms = value->_data->factory_methods;
+					const auto fit = fms.find(arg_types);
+
+					if (fit == fms.end())
 					{
-						return new T(args...);
-					};
-					fms[arg_types] = reinterpret_cast<fnptr>(temp);
+						void* (*temp)() =
+							[]() -> void*
+						{
+							return new T();
+						};
+
+						fms[arg_types] = reinterpret_cast<fnptr>(temp);
+					}
 				}
 
-				auto value = new type(data);
-
-				__instances[typeid(T)] = value;
 				return *value;
 			}
 
@@ -105,7 +124,7 @@ namespace zx
 				}
 			}
 
-			throw std::logic_error("No factory method during create_instance. ");
+			throw std::logic_error("No factory method during instantiate call. ");
 		}
 
 		ZX_API bool operator ==(const type& other) const;
@@ -116,22 +135,65 @@ namespace zx
 		ZX_API type(type_data *data);
 	};
 
-	template <class Signature>
-	class reflect;
-
-	template <class Ret, class... Args>
-	class reflect<Ret(Args...)>
+	template<typename T>
+	struct refl
 	{
+		template<typename... Sig>
+		struct ctor;
 
-	public:
-		type operator()() const
+		template<typename First, typename... Args>
+		struct ctor <First, Args...>
 		{
-			return type::i<Ret, Args...>();
-		}
+			static_assert(std::is_constructible_v<T, First, Args...>, "Type is not constructible from these args. ");
 
-		operator type() const
+			static rtti_args make_args()
+			{
+				return rtti_pack<First, Args...>::get();
+			}
+
+			static fnptr make_fnptr()
+			{
+				void* (*temp)(First, Args...) =
+					[](First first, Args... args) -> void*
+				{
+					return new T(first, args...);
+				};
+
+				return reinterpret_cast<fnptr>(temp);
+			}
+
+			static const zx::type& ensure()
+			{
+				return type::ensure<T, zx::refl<T>::template ctor<First, Args...>>();
+			}
+		};
+
+		template<>
+		struct ctor <>
 		{
-			return type::i<Ret, Args...>();
-		}
+			static_assert(std::is_default_constructible_v<T>, "Type is not default constructible. ");
+
+			static rtti_args make_args()
+			{
+				return rtti_pack<>::get();
+			}
+
+			static fnptr make_fnptr()
+			{
+				void* (*temp)() =
+					[]() -> void*
+				{
+					return new T();
+				};
+
+				return reinterpret_cast<fnptr>(temp);
+			}
+
+			static const zx::type& ensure()
+			{
+				return type::ensure<T, zx::refl<T>::template ctor<>>();
+			}
+		};
 	};
 }
+
--- core_test/ReflectionTest/ReflectionTest.cpp
+++ core_test/ReflectionTest/ReflectionTest.cpp
@@ -16,32 +16,42 @@ namespace zx_test
 			Assert::AreEqual(sizeof(zx::type), 40ui64);
 
 			auto& int_type = zx::type::i<int32_t>();
-			auto new_int = 
+			auto new_int =
 				std::unique_ptr<int32_t>(
 					static_cast<int32_t*>(int_type.instantiate()));
+
+			Assert::AreEqual(0i32, *new_int);
 		}
 
 		/// Instantiates vector of int with size 10
 		TEST_METHOD(InstantiatePerformanceTest)
 		{
 			constexpr zx::u64 size = 10ui64;
-			auto& vector_type = zx::type::i<std::vector<zx::i32>, zx::u64>();
+
+			// Default ctor will be added to metadata of zx::type
+			// if type is default constructible. 
+			const zx::type& vector_type = zx::type::i<std::vector<zx::i32>>();
+			
+			// Ensure all non-default ctors you need. Manually :(
+			zx::refl<std::vector<zx::i32>>::ctor<zx::u64>::ensure();
 
 			// Performance test
 			constexpr zx::u64 instance_amount = 1000ui64;
-			void* pointers[instance_amount] {};
+			void* pointers[instance_amount]{};
 
 			auto before = std::chrono::high_resolution_clock::now();
 
 			// Instantiate number of instances. 
 			for (auto i = zx::zero::u64; i < instance_amount; ++i)
 			{
-				pointers[i] = new std::vector<int>(10);
+				pointers[i] = new std::vector<zx::i32>(size);
 			}
 
 			auto after = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double, std::milli> spent_for_new = after - before;
-			ls << "[new T()]: " << zx::endl
+			std::chrono::duration<double, std::milli> 
+				spent_for_new = after - before;
+
+			ls  << "[new T()]: " << zx::endl
 				<< "Instantiated " << instance_amount << " "
 				<< "instances of type \"" << vector_type.name << "\". "
 				<< "Time spent: " << spent_for_new << "ms. " << zx::endl;
@@ -70,7 +80,7 @@ namespace zx_test
 
 			after = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double, std::milli> spent_for_inst = after - before;
-			ls	<< "[zx::type::instantiate()]: " << zx::endl 
+			ls  << "[zx::type::instantiate()]: " << zx::endl
 				<< "Instantiated " << instance_amount << " "
 				<< "instances of type \"" << vector_type.name << "\". "
 				<< "Time spent: " << spent_for_inst << "ms. " << zx::endl;
@@ -89,7 +99,8 @@ namespace zx_test
 				Assert::AreEqual(size, new_vector->size());
 			}
 
-			ls	<< spent_for_inst / spent_for_new 
+			ls << spent_for_inst / spent_for_new
 				<< " times slower than new(). ";
 		}
+	};
 }
